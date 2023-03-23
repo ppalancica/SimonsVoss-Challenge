bcacd30b0dc9956d3da3f78307f841ecdae6f3b0
Pavel Palancica <pavel.palancica@gmail.com>
2023-03-23 22:08:47 +0200

    Removed generated Main.storyboard and ViewController.swift files. Removed generated test files. Some cleanups

--- SimonsVoss-Challenge/SceneDelegate.swift
+++ SimonsVoss-Challenge/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  SimonsVoss-Challenge
 //
-//  Created by M1 Pro on 23.03.2023.
+//  Created by Pavel Palancica on 23.03.2023.
 //
 
 import UIKit
@@ -11,8 +11,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
@@ -46,7 +47,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
