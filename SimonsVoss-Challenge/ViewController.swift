71bf2ee3dff37c24917340ef94a43ada02969f4c
Pavel Palancica <pavel.palancica@gmail.com>
2023-03-23 21:45:21 +0200

    First commit. Created new project in Xcode using iOS App template

---
+++ SimonsVoss-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimonsVoss-Challenge
+//
+//  Created by M1 Pro on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
