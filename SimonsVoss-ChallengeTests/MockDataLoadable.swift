3489737a865d4206b6f89cbf86025e40f76707a6
Pavel Palancica <pavel.palancica@gmail.com>
2023-04-02 18:29:50 +0300

    Created MockDataLoadable protocol to load local JSON file. Implemented that protocol from MockHTTPClient

---
+++ SimonsVoss-ChallengeTests/MockDataLoadable.swift
@@ -0,0 +1,36 @@
+//
+//  MockDataLoadable.swift
+//  SimonsVoss-ChallengeTests
+//
+//  Created by Pavel Palancica on 02.04.2023.
+//
+
+import Foundation
+
+protocol MockDataLoadable: AnyObject {
+    
+    var bundle: Bundle { get }
+    
+    func loadJSON(fileName: String) -> Data
+}
+
+extension MockDataLoadable {
+    
+    var bundle: Bundle {
+        return Bundle(for: type(of: self))
+    }
+
+    func loadJSON(fileName: String) -> Data {
+        guard let path = bundle.url(forResource: fileName, withExtension: "json") else {
+            fatalError("Failed to load Test Bundle JSON file")
+        }
+        
+        do {
+            let data = try Data(contentsOf: path)
+            return data
+        } catch {
+            print("ERROR: \(error)")
+            fatalError("Failed to decode the JSON from \(bundle)")
+        }
+    }
+}
---
+++ SimonsVoss-ChallengeTests/MockHTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  MockHTTPClient.swift
+//  SimonsVoss-ChallengeTests
+//
+//  Created by Pavel Palancica on 02.04.2023.
+//
+
+import Foundation
+@testable import SimonsVoss_Challenge
+
+final class MockHTTPClient: HTTPClientType, MockDataLoadable {
+    
+    func getData(from url: URL) async throws -> Data {
+        return loadJSON(fileName: "RootPageResponse")
+    }
+}
